fa9fbddb53b472d14cf08f18170745011fb0551d
dwissaaj <dwisetyoaji@jrdataanalyst.com>
2023-05-28 20:11:54 +0700

    add dabatase

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 module.exports = ({env}) => ({
   defaultConnection: "default",
   connection: {
     client: 'postgres',
+    connectionString: env('DATABASE_CONNECTION_STRING'),
     connection: {
       host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT'),
@@ -9,6 +11,7 @@ module.exports = ({env}) => ({
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
       schema: env('DATABASE_SCHEMA'),
+
     }
   }
 })
