7763839d2b37cb440d9016121e1ae6adf8547d6b
dwissaaj <dwisetyoaji@jrdataanalyst.com>
2023-06-07 18:56:57 +0700

    fix bug at #2 :bug:

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,11 +2,11 @@ module.exports = [
   {
     name: "strapi::body",
     config: {
-      formLimit: "25mb", // modify form body
-      jsonLimit: "25mb", // modify JSON body
-      textLimit: "25mb", // modify text body
+      formLimit: "5mb", // modify form body
+      jsonLimit: "5mb", // modify JSON body
+      textLimit: "5mb", // modify text body
       formidable: {
-        maxFileSize: 25 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+        maxFileSize: 5 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
       },
     },
 
