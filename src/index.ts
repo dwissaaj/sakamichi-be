aeffb5227b8b06c42bfa5136a4b6cbc4d0c8f04e
dwissaaj <dwisaji150@gmail.com>
2025-01-16 20:44:02 +0700

    add prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Hono } from 'hono'
-
-const app = new Hono()
+import single from './single/single'
+type Bindings = {
+  DB: D1Database
+}
+const app = new Hono<{Bindings: Bindings}>().basePath('/api')
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Sakamichi Fans!use this api wisely!')
 })
+app.route('/single', single)
 
 export default app
