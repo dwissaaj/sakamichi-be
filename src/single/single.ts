f24c7d1334018f7b316930f91a8cf0bb5d2f6dec
dwissaaj <dwisaji150@gmail.com>
2025-01-23 14:15:40 +0700

    fix api single

--- src/single/single.ts
+++ src/single/single.ts
@@ -2,9 +2,11 @@ import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import prismaClients from "../../lib/prisma";
 import image from "./image/image";
-import { PrismaClientKnownRequestError, PrismaClientValidationError } from "@prisma/client/runtime/library";
-
+import {
+  PrismaClientKnownRequestError,
+} from "@prisma/client/runtime/library";
 const single = new Hono();
+
 single.get("/all", async (c) => {
   try {
     const prisma = await prismaClients.prisma(c);
@@ -14,6 +16,7 @@ single.get("/all", async (c) => {
     throw new HTTPException(401, { message: "Error" });
   }
 });
+
 single.get("/get/:id", async (c) => {
   const id = c.req.param("id");
   try {
@@ -29,11 +32,10 @@ single.get("/get/:id", async (c) => {
     return c.json({ single });
   } catch (error) {
     console.log("Error at /single/get unable to get data", error);
-    const e = error as PrismaClientValidationError;
+    const e = error as PrismaClientKnownRequestError;
     throw new HTTPException(400, { message: `${e.message}` });
   }
 });
-
 single.post("/upload", async (c) => {
   const data = await c.req.json();
   try {
@@ -41,25 +43,20 @@ single.post("/upload", async (c) => {
     const single = await prisma.single.create({
       data: data,
     });
-    return c.json({ single });
+    return c.json( {single} );
   } catch (error) {
     console.error("Error at publish a single", error);
-    const e = error as PrismaClientValidationError;
-    if (e.name === "P2002") {
+    const e = error as PrismaClientKnownRequestError;
+    if (e.code === "P2002") {
       throw new HTTPException(400, {
         message: `${e.message}`,
-        cause: `${e.cause}`,
+        cause: `There is data with same ID alias Duplicate`,
       });
     }
-    if (e.name === "P1012") {
-      throw new HTTPException(400, {
-        message: `${e.message}`,
-        cause: `${e.cause}`,
-      });
-    }
+
     throw new HTTPException(400, {
       message: `${e.message}`,
-      cause: "General Error",
+      cause: `${e.cause}`,
     });
   }
 });
@@ -67,12 +64,12 @@ single.delete("/remove/:id", async (c) => {
   const id = c.req.param("id");
   const prisma = await prismaClients.prisma(c);
   try {
-    
-    await prisma.single.delete({
+    const single = await prisma.single.delete({
       where: {
         id: id,
       },
     });
+
     return c.text("Good! Data is Removed");
   } catch (error) {
     console.log("Log: Delete Single Error", error);
@@ -80,11 +77,12 @@ single.delete("/remove/:id", async (c) => {
     if (e.code === "P2025") {
       throw new HTTPException(400, {
         message: "Your data have been removed",
-        cause: "Wrongfully add deleted ID as params",
+        cause: "Your Record have been deleted",
       });
     }
     throw new HTTPException(400, {
-      message: "There is no such data with that ID",
+      message: `${e.message}`,
+      cause: `${e.cause}`
     });
   }
 });
@@ -93,25 +91,28 @@ single.patch("/update/:id", async (c) => {
   const prisma = await prismaClients.prisma(c);
   const body = await c.req.json();
   try {
-    const single = await prisma.single.update( {
+    const single = await prisma.single.update({
       where: {
-        id: id
+        id: id,
       },
-      data: body
-    })
+      data: body,
+    });
 
-    return c.json({single})
+    return c.json({ single });
   } catch (error) {
-    const e = error as PrismaClientKnownRequestError
-    console.log("LOG101: Error at Update", e)
+    const e = error as PrismaClientKnownRequestError;
+    console.log("LOG101: Error at Update", e);
     if (e.code === "P2025") {
       throw new HTTPException(400, {
-        message: "Your data have been removed",
-        cause: "Wrongfully add deleted ID as params",
+        message: `${e.message}`,
+        cause: "There is no record with that ID",
       });
     }
-    
+    throw new HTTPException(400, {
+      message: `${e.message}`,
+      cause: `${e.cause}`,
+    });
   }
-})
+});
 single.route("/image", image);
 export default single;
